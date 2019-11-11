be0434d366b84af032e51bed98ab9ba200628ec9
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-11 16:33:23 -0500

    set view

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -1,10 +1,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var labelForGameName: UILabel!
+    
+    @IBOutlet weak var labelToSelectTheSuit: UILabel!
+    
+    @IBOutlet weak var changeSuite: UISegmentedControl!
+    
+    
+    @IBOutlet weak var labelForSteppe: UILabel!
+    
+    @IBOutlet weak var stepperItself: UIStepper!
+    
+   
+    @IBOutlet weak var cardDisplayArea: UIView!
+    
+    
+    @IBOutlet weak var cardValueLabel: UILabel!
+    
+    
+    @IBOutlet weak var suitLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cardDisplayArea.layer.borderColor = UIColor.black.cgColor
     }
+    
+    @IBAction func actionToSelectSuit(_ sender: UISegmentedControl) {
+    }
+    @IBAction func actionToSelectValue(_ sender: UIStepper) {
+    }
+    
+    
+    
 }
 
