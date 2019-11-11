2f9c61c38ce18f8660017bfef883239c839c7e21
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-11-11 17:36:08 -0500

    completed Lab

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,9 +2,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Outlets and properties
+    
+    //controls
+    @IBOutlet weak var suitSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var valueStepperControl: UIStepper!
+    
+    //card labels
+    @IBOutlet var cardLabels: [UILabel]!
+    
+    var currentCardSuit = "♦️"
+    var currentCardValue = "A"
+    var cardSuit = ["♦️", "♥️", "♣️", "♠️"]
+    var cardValues = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    
+    //MARK:- View Controller Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureStepper()
+        for cardLabel in cardLabels{
+            cardLabel.text = "\(currentCardValue)\(currentCardSuit)"
+        }
     }
+    
+    //helper function
+    func configureStepper(){
+        
+        valueStepperControl.minimumValue = Double(cardValues.startIndex)
+        valueStepperControl.maximumValue = Double(cardValues.count) - 1.0
+        valueStepperControl.stepValue = 1.0
+        
+        //default start value
+        valueStepperControl.value = 0.0
+    }
+    
+    //MARK:- Actions and Methods
+    
+    @IBAction func suitSegmentedControllerChanged(_ sender: UISegmentedControl) {
+        let currentCardSuitIndex = sender.selectedSegmentIndex
+        currentCardSuit = cardSuit[currentCardSuitIndex]
+        
+        for cardLabel in cardLabels{
+            cardLabel.text = "\(currentCardValue)\(currentCardSuit)"
+        }
+        
+    }
+    @IBAction func valueStepperControlChanged(_ sender: UIStepper) {
+        let currentCardValueIndex = Int(sender.value)
+        currentCardValue = cardValues[currentCardValueIndex]
+        
+        for cardLabel in cardLabels{
+            cardLabel.text = "\(currentCardValue)\(currentCardSuit)"
+        }
+    }
+    
 }
 
